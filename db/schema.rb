c1c94a7c4354c7336b2eee1faa8445eac9143903
monicaw218 <monicadweitekamp@gmail.com>
2022-06-19 18:59:19 -0400

    Introduce user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_17_151358) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_19_225618) do
   create_table "beers", force: :cascade do |t|
     t.string "brand"
     t.string "style"
     t.string "country"
     t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
