5e3500f44e63679302dd186a746499f05f55fe16
monicaw218 <monicadweitekamp@gmail.com>
2022-06-20 17:20:23 -0400

    Include bootstrap css for the whole app

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '../components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
