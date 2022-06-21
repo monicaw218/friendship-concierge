972325384e5cd99c1431af5f6e8616c05a36ec9b
monicaw218 <monicadweitekamp@gmail.com>
2022-06-21 17:10:23 -0400

    Put content into a layout

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Routes from '../routes/index';
 import 'antd/dist/antd.css';
+import { Layout } from 'antd';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const { Content } = Layout;
 
 export default () => (
-	<>
+  <Layout className="layout">
 		<Header />
-		{Routes}
-	</>
+		<Content style={{ padding: "0 50px" }}>
+  		<div className="site-layout-content" style={{ margin: "100px auto" }}>
+				{Routes}  			
+  		</div>
+  	</Content>
+		<Footer />
+	</Layout>
 );
