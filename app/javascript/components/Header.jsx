0f45076f66f8d259e3ba9dc0e532b3ee1c249dfc
monicaw218 <monicadweitekamp@gmail.com>
2022-06-21 15:55:21 -0400

    Use suggested atnd menu syntax

--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -3,19 +3,28 @@ import { Layout, Menu } from 'antd';
 import { Link } from 'react-router-dom';
 const { Header } = Layout;
 
+const makeLabel = (url, title) => {
+  return (<a href={url}>{title}</a>)
+};
+
+const menuItems = [
+  {
+    key: '/',
+    label: makeLabel('/', 'Home'),
+  },
+  {
+    key: '/signup',
+    label: makeLabel('/signup', 'Sign Up'),
+  },
+  {
+    key: '/contact',
+    label: makeLabel('/contact', 'Contact'),
+  },
+];
+
 export default () => (
   <Header>
     <div className='logo' />
-    <Menu theme='dark' mode='horizontal'>
-      <Menu.Item key='1'>
-        <a href='/'>Home</a>
-      </Menu.Item>
-      <Menu.Item key='2'>
-        <a href='/signup'>Sign Up</a>
-      </Menu.Item>
-      <Menu.Item key='3'>
-        <a href='/contact'>Contact</a>
-      </Menu.Item>
-    </Menu>
+    <Menu theme='dark' mode='horizontal' items={menuItems} />
   </Header>
 );
