92accf0cdac488032ccd5b92265ec965ac899802
monicaw218 <monicadweitekamp@gmail.com>
2022-06-21 17:07:16 -0400

    Footer component

---
+++ app/javascript/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Layout, Menu } from 'antd';
+import { Link } from 'react-router-dom';
+const { Footer } = Layout;
+
+export default () => (
+  <Footer style={{ textAlign: "center" }}>Weitekamp ©2022.</Footer>
+);
