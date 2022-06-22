3c9c27eefebb1ae8bff600c86eb1dea8fec050fd
monicaw218 <monicadweitekamp@gmail.com>
2022-06-22 07:50:15 -0400

    Clean up sign up form, add values

--- app/javascript/components/SignUp.jsx
+++ app/javascript/components/SignUp.jsx
@@ -11,9 +11,7 @@ const SignUp = () => {
   const onSubmit = (event) => {
     event.preventDefault();
     const url = '/api/v1/users/create';
-
     const body = { user: { first_name: firstName, last_name: lastName, username: email, password: password } };
-
     const token = document.querySelector('meta[name="csrf=token"]').content;
 
     fetch(url, {
@@ -39,19 +37,42 @@ const SignUp = () => {
         <h1 className='display-4'>Sign Up</h1>
         <Form style={{ display: 'inline-grid' }}>
           <label>First Name</label>
-          <input type='text' placeholder='Fred' onChange={e => setFirstName(e.target.value)} />
+          <input
+            type='text'
+            placeholder='Fred'
+            onChange={e => setFirstName(e.target.value)}
+            value={firstName}
+          />
 
           <label>Last Name</label>
-          <input type='text' placeholder='Rogers' onChange={e => setLastName(e.target.value)} />
+          <input
+            type='text'
+            placeholder='Rogers'
+            onChange={e => setLastName(e.target.value)}
+            value={lastName}
+          />
 
           <label>Email</label>
-          <input type='email' placeholder='frogers@neighborhood.com' onChange={e => setEmail(e.target.value)} />
+          <input
+            type='email'
+            placeholder='frogers@neighborhood.com'
+            onChange={e => setEmail(e.target.value)}
+            value={email}
+          />
 
           <label>Password</label>
-          <input type='password' placeholder='Set password' onChange={e => setPassword(e.target.value)} />
+          <input
+            type='password'
+            placeholder='Set password'
+            onChange={e => setPassword(e.target.value)}
+          />
 
-          <label>Password Confirmation</label>
-          <input type='password' placeholder='Confirm password' onChange={e => setPasswordConfirmation(e.target.value)} />
+          <label>Confirm Password</label>
+          <input
+            type='password'
+            placeholder='Confirm password'
+            onChange={e => setPasswordConfirmation(e.target.value)}
+          />
 
           <button type='submit' className='btn custom-button mt-3' onClick={e => onSubmit(e)}>
             Submit
