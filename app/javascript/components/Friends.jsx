93f7c86746a90a60c2e982e5b97732e9afdb23ed
monicaw218 <monicadweitekamp@gmail.com>
2022-06-22 00:20:58 -0400

    Turn Friends into a functional component
    Semistandard for Friends component
    
    A note about useEffect in the friends component so I don't forget

--- app/javascript/components/Friends.jsx
+++ app/javascript/components/Friends.jsx
@@ -1,108 +1,101 @@
-import { Table, message, Popconfirm } from "antd";
-import React from "react";
-import AddFriendModal from "./AddFriendModal";
+import { Table, message, Popconfirm } from 'antd';
+import React, { useState, useEffect } from 'react';
+import AddFriendModal from './AddFriendModal';
 
-class Friends extends React.Component {
-  columns = [
+const Friends = () => {
+  const [friends, setFriends] = useState([]);
+
+  // passing an empty array as last arg ensures loadFriends is only called the first time the component loads
+  useEffect(() => { loadFriends(); }, []);
+
+  const columns = [
     {
-      title: "First Name",
-      dataIndex: "first_name",
-      key: "first_name",
+      title: 'First Name',
+      dataIndex: 'first_name',
+      key: 'first_name'
     },
     {
-      title: "Last Name",
-      dataIndex: "last_name",
-      key: "last_name",
+      title: 'Last Name',
+      dataIndex: 'last_name',
+      key: 'last_name'
     },
     {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age'
     },
     {
-      title: "Interests",
-      dataIndex: "interests",
-      key: "interests",
+      title: 'Interests',
+      dataIndex: 'interests',
+      key: 'interests'
     },
     {
-      title: "",
-      key: "action",
+      title: '',
+      key: 'action',
       render: (_text, record) => (
-        <Popconfirm title="Are you sure to delete this friend?" onConfirm={() => this.deleteFriend(record.id)} okText="Yes" cancelText="No">
-          <a href="#" type="danger">
-            Delete{" "}
+        <Popconfirm title='Are you sure to delete this friend?' onConfirm={() => deleteFriend(record.id)} okText='Yes' cancelText='No'>
+          <a href='#' type='danger'>
+            Delete{' '}
           </a>
         </Popconfirm>
-      ),
-    },
+      )
+    }
   ];
 
-  state = {
-    friends: [],
-  };
-
-  componentDidMount() {
-    this.loadFriends();
-  }
-
-  loadFriends = () => {
-    const url = "api/v1/friends/index";
+  const loadFriends = () => {
+    const url = 'api/v1/friends/index';
     fetch(url)
       .then((data) => {
         if (data.ok) {
           return data.json();
         }
-        throw new Error("Network error.");
+        throw new Error('Network error.');
       })
       .then((data) => {
         data.forEach((friend) => {
-          const newEl = {
+          const newFriend = {
             key: friend.id,
             id: friend.id,
             first_name: friend.first_name,
             last_name: friend.last_name,
             age: friend.age,
-            interests: friend.interests,
+            interests: friend.interests
           };
 
-          this.setState((prevState) => ({
-            friends: [...prevState.friends, newEl],
-          }));
+          setFriends(friends => [...friends, newFriend]);
         });
       })
-      .catch((err) => message.error("Error: " + err));
+      .catch((err) => message.error('Error: ' + err));
   };
 
-  reloadFriends = () => {
-    this.setState({ friends: [] });
-    this.loadFriends();
+  const reloadFriends = () => {
+    setFriends([]);
+    loadFriends();
   };
 
-  deleteFriend = (id) => {
+  const deleteFriend = (id) => {
     const url = `api/v1/friends/${id}`;
 
     fetch(url, {
-      method: "delete",
+      method: 'delete'
     })
       .then((data) => {
         if (data.ok) {
-          this.reloadFriends();
+          reloadFriends();
           return data.json();
         }
-        throw new Error("Network error.");
+        throw new Error('Network error.');
       })
-      .catch((err) => message.error("Error: " + err));
+      .catch((err) => message.error('Error: ' + err));
   };
 
-  render() {
-    return (
-      <>
-        <Table className="table-striped-rows" dataSource={this.state.friends} columns={this.columns} pagination={{ pageSize: 5 }} />
-        
-        <AddFriendModal reloadFriends={this.reloadFriends} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Table className='table-striped-rows' dataSource={friends} columns={columns} pagination={{ pageSize: 5 }} />
+
+      <AddFriendModal reloadFriends={reloadFriends} />
+    </>
+  );
+};
 
 export default Friends;
