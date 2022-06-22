75f00f505f1d132205c4893ad4bad8422e64995f
monicaw218 <monicadweitekamp@gmail.com>
2022-06-22 09:45:24 -0400

    Test user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+	validates :first_name, presence: true, length: { maximum: 25 }
+	validates :last_name, presence: true, length: { maximum: 25 }
+	validates :username, presence: true, length: { maximum: 255 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,47 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(first_name: "Example", last_name: "User", username: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "first name should be present" do
+    @user.first_name = ''
+
+    assert_not @user.valid?
+  end
+
+  test "last name should be present" do
+    @user.last_name = ''
+
+    assert_not @user.valid?
+  end
+
+  test 'email/username should be present' do
+    @user.username = ''
+
+    assert_not @user.valid?
+  end
+
+  test 'first name is not too long' do 
+    @user.first_name = 'a' * 26
+
+    assert_not @user.valid?
+  end
+
+  test 'last name is not too long' do 
+    @user.last_name = 'a' * 26
+
+    assert_not @user.valid?
+  end
+
+  test 'username is not too long' do 
+    @user.username = 'a' * 244 + '@example.com'
+
+    assert_not @user.valid?
+  end
 end
