2c92db9b073032307b4428f670c808da9641c0ef
Monica Weitekamp <monicadweitekamp@gmail.com>
2022-06-22 12:32:44 -0400

    Adapt to new users api controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,23 @@ Rails.application.routes.draw do
 
   resources :users
   resources :friends
+
   namespace :api do
     namespace :v1 do
       get 'friends/index'
       post 'friends/create'
       delete 'friends/:id', to: 'friends#destroy'
+
+      get 'users/index'
+      post 'users/create'
+      delete 'users/:id', to: 'users#destroy'
     end
   end
 
   get '/signup', to: 'users#new'
   get '/contact', to: 'static_pages#contact'
   get '/friends', to: 'friends#index'
+  get '/users', to: 'users#index'
 
   root "static_pages#home"
 end
