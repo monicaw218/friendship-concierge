295879921aa2a6758a3782f6c6473f8f6886bb4e
monicaw218 <monicadweitekamp@gmail.com>
2022-06-22 10:43:11 -0400

    Validate uniqueness of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
 	validates :email, presence: true, length: { maximum: 255 }
 	
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+	validates :email, presence: true, length: { maximum: 255 },
+						format: { with: VALID_EMAIL_REGEX },
+						uniqueness: { case_sensitive: false }
 end
