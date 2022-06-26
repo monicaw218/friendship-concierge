b3b656e66fa28ecb720fbabbbba915d9ef06e282
Monica Weitekamp <monicadweitekamp@gmail.com>
2022-06-26 14:02:40 -0400

    Update app slogan

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -17,7 +17,7 @@ export default () => (
     <div className='jumbotron jumbotron-fluid bg-transparent'>
       <div className='container secondary-color'>
         <h1 className='display-4'>Friendship Concierge</h1>
-        <p className='lead'>A place to reconnect</p>
+        <p className='lead'>Be the friend you always wanted</p>
         <hr className='my-4' />
 
         <Link
