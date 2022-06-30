28a8c7db4b42314ea42f23225cc073f6f792a015
Monica Weitekamp <monicadweitekamp@gmail.com>
2022-06-30 08:52:20 -0400

    Test valid signup information

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,4 +12,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                                 password_confirmation: "bar" } }
     end
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count' do
+      post api_v1_users_path, params: { user: { first_name: "First",
+                                                last_name: "User",
+                                                email: "user@valid.com",
+                                                password: "password123",
+                                                password_confirmation: "password123" } }
+    end
+
+    assert flash[:success]
+  end
 end
