8d244a9f0293646f516fe54405c4d5a9ff643f3a
Monica Weitekamp <monicadweitekamp@gmail.com>
2022-06-30 16:54:10 -0400

    Log in upon sign-up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -33,6 +33,8 @@ class Api::V1::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      reset_session
+      log_in @user
       flash[:success] = "Welcome to the Friendship Concierge!"
       render json: @user
     else
