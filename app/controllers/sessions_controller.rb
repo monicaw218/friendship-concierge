4d734239852d150e58e9b2d2daf767d2ae7fd218
Monica Weitekamp <monicadweitekamp@gmail.com>
2022-06-30 13:15:48 -0400

    Introduce login helper methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      reset_session
+      log_in user
       flash[:success] = 'Login successful'
       render json: user.id
     else
