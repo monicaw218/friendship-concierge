8d3a5def0dbbbfa9edcfc3fe48dbdcabe47b97af
Monica Weitekamp <monicadweitekamp@gmail.com>
2022-06-30 22:59:04 -0400

    Associate each friend with a user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_22_144816) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_01_025602) do
   create_table "friends", force: :cascade do |t|
     t.string "nickname"
     t.integer "age"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_22_144816) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
