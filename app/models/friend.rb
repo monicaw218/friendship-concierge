99fd8981784ae296384c95b95c70f1d4afe493be
Monica Weitekamp <monicadweitekamp@gmail.com>
2022-07-01 22:48:26 -0400

    Validate presence of name for a friend

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,6 +1,10 @@
 class Friend < ApplicationRecord
 	has_many :friend_histories, dependent: :destroy
 
+	validates :first_name, presence: true, length: { maximum: 25 }
+	validates :last_name, presence: true, length: { maximum: 25 }
+	validates_inclusion_of :age, :in => 0..130
+
 	def full_name
 		"#{first_name} #{last_name}"
 	end
