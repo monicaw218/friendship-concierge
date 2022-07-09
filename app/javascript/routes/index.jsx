f5e5dd2208a182320c3f07eb9e048b0e56ae6485
Monica Weitekamp <monicadweitekamp@gmail.com>
2022-07-09 15:15:40 -0400

    Do not show users page until app uses admin access

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -3,24 +3,24 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import Home from '../components/Home';
 import SignUp from '../components/SignUp';
 import Friends from '../components/Friends';
-import Users from '../components/Users';
 import User from '../components/User';
 import Friend from '../components/Friend';
 import Login from '../components/Login';
+import NotFound from '../components/NotFound';
 import ForgotPassword from '../components/ForgotPassword';
 import PasswordReset from '../components/PasswordReset';
 
 const CustomRoutes = ({ loggedIn }) => {
-
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home isLoggedIn={loggedIn} />} />
         <Route path='/signup' element={<SignUp />} />
-        <Route path='/friends' element={loggedIn ? (<Friends />) : (<Navigate to="/login" />)} />
-        <Route path='/users' element={loggedIn ? <Users /> : (<Navigate to="/login" />)} />
-        <Route path='/users/:id' element={loggedIn ? (<User />) : (<Navigate to="/login" />)} />
-        <Route path='/friends/:id' element={loggedIn ? (<Friend />) : (<Navigate to="/login" />)}/>
+        <Route path='/friends' element={loggedIn ? (<Friends />) : (<Navigate to='/login' />)} />
+        {/* TODO: eBFhfXfR - Do not show users page until app uses admin access */}
+        <Route path='/users' element={loggedIn ? <NotFound /> : (<Navigate to='/login' />)} />
+        <Route path='/users/:id' element={loggedIn ? (<User />) : (<Navigate to='/login' />)} />
+        <Route path='/friends/:id' element={loggedIn ? (<Friend />) : (<Navigate to='/login' />)} />
         <Route path='/login' element={<Login />} />
         <Route path='/password_resets/new' element={<ForgotPassword />} />
         <Route path='/password_resets/:digest/edit' element={<PasswordReset />} />
