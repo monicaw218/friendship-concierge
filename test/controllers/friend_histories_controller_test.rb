d0fed824d97a222737000462f09730ea194987a4
Monica Weitekamp <monicadweitekamp@gmail.com>
2022-07-08 21:17:26 -0400

    Test creating a friend history

--- test/controllers/friend_histories_controller_test.rb
+++ test/controllers/friend_histories_controller_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class FriendHistoriesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @friend = friends(:one)
+    @user = users(:monica)
+  end
+
+  test 'should create new' do
+    log_in_as @user
+    assert_difference('FriendHistory.count') do
+      post friend_histories_url, params: { friend_history: {
+        friend_id: 1,
+        description: 'Got a puppy today!'
+      } }
+    end
+
+    friend_history = @response.parsed_body
+    assert_equal friend_history['friend_id'], 1
+    assert_equal friend_history['description'], 'Got a puppy today!'
+  end
 end
