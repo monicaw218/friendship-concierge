20d6e6c19cb4fb42b723770ba743353aea3025db
Monica Weitekamp <monicadweitekamp@gmail.com>
2022-07-08 22:38:31 -0400

    Test to delete a friend history

--- test/controllers/friend_histories_controller_test.rb
+++ test/controllers/friend_histories_controller_test.rb
@@ -1,9 +1,10 @@
-require "test_helper"
+require 'test_helper'
 
 class FriendHistoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @friend = friends(:one)
     @user = users(:monica)
+    @friend_history = FriendHistory.create!(friend_id: @friend.id, description: 'Went to Aruba last month')
   end
 
   test 'should create new' do
@@ -18,5 +19,11 @@ class FriendHistoriesControllerTest < ActionDispatch::IntegrationTest
     friend_history = @response.parsed_body
     assert_equal friend_history['friend_id'], 1
     assert_equal friend_history['description'], 'Got a puppy today!'
+  end
+
+  test 'should destroy friend history' do
+    assert_difference('FriendHistory.count', -1) do
+      delete friend_history_url(@friend_history)
+    end
   end
 end
