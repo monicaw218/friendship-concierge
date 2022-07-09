bfb3b9751186236f518c1da371cf0ed15e1e4a2a
Monica Weitekamp <monicadweitekamp@gmail.com>
2022-07-08 21:32:54 -0400

    Friend history model assertions

--- app/models/friend_history.rb
+++ app/models/friend_history.rb
@@ -2,4 +2,7 @@ class FriendHistory < ApplicationRecord
 	has_one :friend
 
 	default_scope { order(created_at: :desc) }
+
+	validates :friend_id, presence: true
+	validates :description, presence: true
 end
--- test/models/friend_history_test.rb
+++ test/models/friend_history_test.rb
@@ -1,7 +1,22 @@
-require "test_helper"
+require 'test_helper'
 
 class FriendHistoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @friend = friends(:one)
+    @friend_history = FriendHistory.new(friend_id: @friend.id, description: 'cheese')    
+  end
+
+  test 'friend must be valid' do
+    assert @friend_history.valid?    
+  end
+
+  test 'friend history must have a friend id' do
+    @friend_history.friend_id = nil
+    assert_not @friend_history.valid?
+  end
+
+  test 'friend history must have a description' do
+    @friend_history.description = nil
+    assert_not @friend_history.valid?
+  end
 end
