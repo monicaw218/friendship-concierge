819e395e563b8ad541875a6d4609440731890aa0
Monica Weitekamp <monicadweitekamp@gmail.com>
2022-07-08 21:05:36 -0400

    Test creating a user

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,7 +1,23 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+  # setup do
+  #   @user = users(:monica)
   # end
+
+  test 'should create new' do
+    assert_difference('User.count') do
+      post api_v1_users_url, params: { user: {
+        first_name: 'Brenda',
+        last_name: 'Song',
+        email: 'bsong@gmail.com',
+        password: 'password'
+      } }
+    end
+
+    user = @response.parsed_body
+    assert_equal user['first_name'], 'Brenda'
+    assert_equal user['last_name'], 'Song'
+    assert_equal user['email'], 'bsong@gmail.com'
+  end
 end
