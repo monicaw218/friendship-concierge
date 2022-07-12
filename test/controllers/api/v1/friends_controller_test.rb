94c6928f73fcf9b6bdd4bd8b27e5100ab40dbc7d
Monica Weitekamp <monicadweitekamp@gmail.com>
2022-07-12 19:17:58 -0400

    Test to update friend

--- test/controllers/api/v1/friends_controller_test.rb
+++ test/controllers/api/v1/friends_controller_test.rb
@@ -40,19 +40,18 @@ class Api::V1::FriendsControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  # @WIP - mweitekamp, 6/21/2022
-  # test "should update friend" do
-  # patch friend_url(@friend), params: { friend: {
-  #   address_line1: @friend.address_line1,
-  #   address_line2: @friend.address_line2,
-  #   age: @friend.age,
-  #   city: @friend.city,
-  #   interests: @friend.interests,
-  #   name: @friend.name,
-  #   nickname: @friend.nickname,
-  #   state: @friend.state,
-  #   zip: @friend.zip
-  # } }
-  #   assert_redirected_to friend_url(@friend)
-  # end
+  test 'should update friend' do
+    patch api_v1_friend_url(@friend), params: { friend: {
+      age: 42,
+      interests: 'Dentistry, marine life',
+      first_name: 'Patrick',
+      last_name: 'Sherman'
+    } }
+
+    friend = @response.parsed_body
+    assert_equal 'Patrick', friend['first_name']
+    assert_equal 'Sherman', friend['last_name']
+    assert_equal 42, friend['age']
+    assert_equal 'Dentistry, marine life', friend['interests']
+  end
 end
